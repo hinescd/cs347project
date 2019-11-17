0b7855980929cf93edddb69104beba8d8d1e6de2
Geoffrey A Wright <wrigh2ga@dukes.jmu.edu>
2019-11-17 17:24:19 -0500

    Forum displays properly, need to add search, ask, and answer functionality.

--- php/class_forum.php
+++ php/class_forum.php
@@ -1,43 +1,46 @@
-<?php
-require_once('db_connection.php');
-$connection = OpenCon();
+<div id="class_forum">
+    <?php
+    require_once('db_connection.php');
+    $connection = OpenCon();
 
-# Query classes and store them
-$classQuesQuery = "SELECT classforum.forumID, className, questionID, title, details, asked, author FROM classforum JOIN question ON classforum.forumID = question.forumID WHERE classforum.forumID = ".$_POST['class']." ORDER BY questionID DESC";
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $connection->connect_error);
-    exit();
-}
-$questions = array();
-if ($result = $connection->query($classQuesQuery)) {
-    while($item = $result->fetch_assoc()) {
-        $questions[] = $item;
+    # Query classes and store them
+    $classQuesQuery = "SELECT classforum.forumID, className, questionID, title, details, asked, author FROM classforum JOIN question ON classforum.forumID = question.forumID WHERE classforum.forumID = ".$_POST['class']." ORDER BY questionID DESC";
+    if ($connection->connect_errno) {
+        printf("Connect failed: %s\n", $connection->connect_error);
+        exit();
     }
-    /* free result set */
-    $result->close();
-}
+    $questions = array();
+    if ($result = $connection->query($classQuesQuery)) {
+        while($item = $result->fetch_assoc()) {
+            $questions[] = $item;
+        }
+        /* free result set */
+        $result->close();
+    }
 
-echo("<table id=\"class_forum\" class=\"table table-striped table-responsive\">");
-echo("<thead class=\"thead-light\">");
-echo("<tr>");
-echo("<th class=\"col\">Title</th>");
-echo("<th class=\"col\">author</th>");
-echo("<th class=\"col\">asked</th>");
-echo("</tr>");
-echo("</thead>");
-echo("<tbody>");
-for ($x = 0; $x < count($questions); $x++) {
-    $qID = $questions[$x]['questionID'];
-    $qTitle = $questions[$x]['title'];
-    $author = $questions[$x]['author'];
-    $asked = $questions[$x]['asked'];
-    echo("<tr id=\"".$qID."\">");
-    echo("<td><h3 class=\"h5\"><a class=\"question_anchor\" href=\"#0\">".$qTitle."</a></h3></td>");
-    echo("<td><div>".$author."</div></td>");
-    echo("<td><div>".$asked."</div></td>");
+    echo("<a href=\"#\" class=\"btn btn-primary\">Ask a Question</a>");
+    echo("<table class=\"table table-striped table-responsive\">");
+    echo("<thead class=\"thead-light\">");
+    echo("<tr>");
+    echo("<th class=\"col\">Title</th>");
+    echo("<th class=\"col\">author</th>");
+    echo("<th class=\"col\">asked</th>");
     echo("</tr>");
-}
-echo("</tbody>");
-?>
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script src="../js/question-page.js"></script>
+    echo("</thead>");
+    echo("<tbody>");
+    for ($x = 0; $x < count($questions); $x++) {
+        $qID = $questions[$x]['questionID'];
+        $qTitle = $questions[$x]['title'];
+        $author = $questions[$x]['author'];
+        $asked = $questions[$x]['asked'];
+        echo("<tr id=\"".$qID."\">");
+        echo("<td><h3 class=\"h5\"><a class=\"question_anchor\" href=\"#0\">".$qTitle."</a></h3></td>");
+        echo("<td><div>".$author."</div></td>");
+        echo("<td><div>".$asked."</div></td>");
+        echo("</tr>");
+    }
+    echo("</tbody>");
+    ?>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="../js/question-page.js"></script>
+</div>
--- php/question-page.php
+++ php/question-page.php
@@ -1,5 +1,44 @@
 <div id='question_page'>
   <?php
-    echo($_POST['question'])
+  require_once('db_connection.php');
+  $connection = OpenCon();
+  
+  # Query for the question forum
+  $questionForumQuery = "SELECT question.questionID, title, details, asked, question.author AS qAuthor, answerID, answer, answered, answer.author FROM question LEFT JOIN answer ON question.questionID = answer.questionID WHERE question.questionID =\"".($_POST['question'])."\"ORDER BY answered DESC";
+  if ($connection->connect_errno) {
+      printf("Connect failed: %s\n", $connection->connect_error);
+      exit();
+  }
+  $questions = array();
+  if ($result = $connection->query($questionForumQuery)) {
+      while($item = $result->fetch_assoc()) {
+          $questions[] = $item;
+      }
+      /* free result set */
+      $result->close();
+  }
+  # most recent answers to oldest
+    echo("<div class=\"card text-center\">");
+    echo("<div class=\"card-header\"><h5 class=\"card-title\">".$questions[0]['title']."</h5></div>");
+    echo("<div class=\"card-body\">"); # Start body
+    echo("<p class=\"card-text\">".$questions[0]['details']."</p>");
+    echo("</div>"); # End body
+    echo("<div class=\"card-footer text-muted\">"); # Start footer
+    echo("<p>".$questions[0]['asked']." -- ".$questions[0]['qAuthor']."</p>");
+    echo("<a href=\"#\" class=\"btn btn-primary\">Answer Question</a>");
+    echo("</div>");
+
+    if (!is_null($questions[0]['answerID'])) {
+      for ($x = 0; $x < count($questions); $x++) {
+        echo("<div class=\"card text-center\">");
+        echo("<div class=\"card-body\">"); # Start body
+        echo("<p class=\"card-text\">".$questions[$x]['answer']."</p>");
+        echo("</div>"); # End body
+        echo("<div class=\"card-footer text-muted\">"); # Start footer
+        echo("<p>".$questions[$x]['answered']." -- ".$questions[$x]['author']."</p>");
+        echo("</div>"); # End footer
+        echo("</div>");
+      }
+    }
   ?>
 </div>
