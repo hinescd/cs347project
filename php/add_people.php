158c4a2d62375fcfb3cc74dc107023c4480cd56d
Robert Verdisco <verdisra@dukes.jmu.edu>
2019-11-20 17:59:19 -0500

    Added functionality to add people tab

---
+++ php/add_people.php
@@ -0,0 +1,35 @@
+
+<?php
+    require_once('db_connection.php');
+    $connection = OpenCon();
+
+    if ($connection->connect_errno) {
+        printf("Connect failed: %s\n", $connection->connect_error);
+        exit();
+    }
+
+    if((!empty($_POST['name'])) && (!empty($_POST['email']))) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $role = $_POST['exampleRadios'];
+        
+        if($role == 'TA') {
+            $course = $_POST['course'];
+            $addPersonQuery = "INSERT INTO person(name, email, role, class) VALUES (\"".$name."\", \"".$email."\", \"".$role."\", \"".$course."\")";
+        } else {
+            $addPersonQuery = "INSERT INTO person(name, email, role) VALUES (\"".$name."\", \"".$email."\", \"".$role."\")";
+        }
+
+        Echo "<html>";
+        if ($connection->query($addPersonQuery)) {
+            Echo "<h1>Person added:</h1>";
+            Echo "<h2>Name: $name</h2>";
+            Echo "<h2>email: $email</h2>";
+            Echo "<h2>role: $role</h2>";
+        } else {
+            echo("Failed to add person into the database.");
+        }
+        Echo "</html>";
+    }
+    CloseCon($connection);
+?>
